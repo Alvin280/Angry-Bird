a0caf07df1f23e6f26737e943048c4592a00a3f0
Alvin280 <70639335+Alvin280@users.noreply.github.com>
2020-10-07 16:36:53 +0530

    Add files via upload

--- Pig.js
+++ Pig.js
@@ -1,8 +1,7 @@
-class Pig extends Baseclass {
-    constructor(x, y) {
-      super(x, y, 50, 50);
-      this.image=loadImage("sprites/enemy.png");
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+  }
 
-    }
-  };
-  
+};
