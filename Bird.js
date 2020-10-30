c7a4482785ce180152dee8da8ee07dcd207bb303
Alvin280 <70639335+Alvin280@users.noreply.github.com>
2020-10-30 16:50:15 +0530

    Add files via upload

--- Bird.js
+++ Bird.js
@@ -2,21 +2,29 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
-    this.smokeImage= loadImage("sprites/smoke.png")
-    this.trajectry=[]
-
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory =[];
+    this.visibility= 255;
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
+
     super.display();
-    if(this.body.velocity.x>10 && this.body.position.x>200){
-    var position=[this.body.position.x,this.body.position.y]
-    this.trajectry.push(position)
+
+    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
     }
-    for (var i = 0; i<this.trajectry.length; i++){
-      image (this.smokeImage,this.trajectry[i][0],this.trajectry [i][1])
+   
+
+    for(var i=0; i<this.trajectory.length; i++){
+      push()
+      this.visibility=this.visibility-5
+      tint(255,this.visibility)
+      image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+      pop()
     }
   }
 }
